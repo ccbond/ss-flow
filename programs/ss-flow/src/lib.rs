77a801632a491d9d9b34a92da6d543fc3f9b5a78
ccbond <1315275049@qq.com>
2024-04-07 00:39:44 +0800

    Fix bug

--- programs/ss-flow/src/lib.rs
+++ programs/ss-flow/src/lib.rs
@@ -8,7 +8,7 @@ use state::{pool::Pool, position::Position};
 use whirlpool_cpi::state::Position as WPosition;
 use whirlpool_cpi::state::{TickArray, Whirlpool};
 
-declare_id!("AsPzpJCRrzHCqZnV3LyykMgLKEBt4oB87XLPC1e15dSp");
+declare_id!("Eh3RT5S8YUbkCG8kfRwZBWETuot3AebXYaZLTgRey5SA");
 
 pub mod errors;
 pub mod event;
