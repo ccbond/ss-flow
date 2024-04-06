97dcba6992e3dfdb4437ddc6e35f81e99c5a5310
ccbond <1315275049@qq.com>
2024-04-06 23:49:54 +0800

    Fix bug

--- programs/ss-flow/src/lib.rs
+++ programs/ss-flow/src/lib.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 use whirlpool_cpi::state::OpenPositionBumps;
 
-declare_id!("8K8ELAz6Q5uvNwQz1iYqn8BnLU1Lf1LFghCTSnBvHAF6");
+declare_id!("Eh3RT5S8YUbkCG8kfRwZBWETuot3AebXYaZLTgRey5SA");
 
 pub mod errors;
 pub mod event;
@@ -10,11 +10,7 @@ pub mod instructions;
 pub mod state;
 pub mod utils;
 
-use instructions::{
-    add_token_a::{self, AddTokenA},
-    initialize_ss_pool::{self, InitializeSSPool},
-    settle::{self, Settle},
-};
+use instructions::*;
 
 #[program]
 pub mod ss_flow {
@@ -25,11 +21,11 @@ pub mod ss_flow {
         amount: u64,
         proportion: u64,
     ) -> Result<()> {
-        return initialize_ss_pool::handler(ctx, amount, proportion);
+        return instructions::initialize_ss_pool::handler(ctx, amount, proportion);
     }
 
     pub fn add_token_a(ctx: Context<AddTokenA>, amount: u64) -> Result<()> {
-        return add_token_a::handler(ctx, amount);
+        return instructions::add_token_a::handler(ctx, amount);
     }
 
     // pub fn refund_token_b(ctx: Context<RefundTokenB>, amount: u64) -> Result<()> {
@@ -42,6 +38,6 @@ pub mod ss_flow {
         liquidity_amount: u128,
         token_max_b: u64,
     ) -> Result<()> {
-        return settle::handler(ctx, precent, liquidity_amount, token_max_b);
+        return instructions::settle::handler(ctx, precent, liquidity_amount, token_max_b);
     }
 }
