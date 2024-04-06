386249100803a9f307dfdb3e70fabf3cb3425356
ccbond <1315275049@qq.com>
2024-04-06 15:19:27 +0800

    Fix bug

--- programs/ss-flow/src/lib.rs
+++ programs/ss-flow/src/lib.rs
@@ -2,19 +2,48 @@ use anchor_lang::prelude::*;
 
 declare_id!("8K8ELAz6Q5uvNwQz1iYqn8BnLU1Lf1LFghCTSnBvHAF6");
 
+pub mod errors;
+pub mod event;
 pub mod instrucions;
 pub mod state;
+pub mod utils;
 
 use instrucions::*;
 
 #[program]
 pub mod ss_flow {
+    use self::{
+        add_token_a::AddTokenA, initialize_pool::InitializePool, refund_token_b::RefundTokenB,
+    };
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_pool(
+        ctx: Context<InitializePool>,
+        amount: u64,
+        proportion: u64,
+    ) -> Result<()> {
+        instrucions::initialize_pool::handler(ctx, amount, proportion)?;
+        Ok(())
+    }
+
+    pub fn add_token_a(ctx: Context<AddTokenA>, amount: u64) -> Result<()> {
+        instrucions::add_token_a::handler(ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn refund_token_b(ctx: Context<RefundTokenB>, amount: u64) -> Result<()> {
+        instrucions::refund_token_b::handler(ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn settle(
+        ctx: Context<Refund>,
+        precent: u64,
+        liquidity_amount: u128,
+        token_max_b: u64,
+    ) -> Result<()> {
+        instrucions::settle::handler(ctx, precent, liquidity_amount, token_max_b)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
