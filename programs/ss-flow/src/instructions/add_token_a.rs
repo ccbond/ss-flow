2b91f2e7ef2d9f929ce5663ded69412400b70c55
ccbond <1315275049@qq.com>
2024-04-06 22:07:37 +0800

    Fix bug

--- programs/ss-flow/src/instructions/add_token_a.rs
+++ programs/ss-flow/src/instructions/add_token_a.rs
@@ -1,6 +1,6 @@
 use crate::event::AddTokenAEvent;
 use crate::state::pool::Pool;
-use crate::utils::token::transfer;
+use crate::utils::token::{transfer, transfer_from_pool_to_user};
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token::{Mint, Token, TokenAccount};
